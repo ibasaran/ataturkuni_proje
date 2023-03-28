02ef43e1bb17fea6b6f2a911c712a36037ed9247
ismail BAŞARAN <basaran.ismaill@gmail.com>
2023-03-28 15:37:53 +0300

    webprojem ilk hali

---
+++ webprojem/src/main/java/com/webprojem/WebprojemApplication.java
@@ -0,0 +1,13 @@
+package com.webprojem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprojemApplication.class, args);
+	}
+
+}
---
+++ webprojem/src/test/java/com/webprojem/WebprojemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webprojem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
